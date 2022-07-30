dfe2b0fc73b412188abb82237967e68c9c23df2d
Johannes Hermans <johannes@dataplace.com.br>
2022-07-30 09:32:24 -0300

    Startup Application

--- src/Dataplace.Imersao.App/MainForm.cs
+++ src/Dataplace.Imersao.App/MainForm.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Dataplace.Imersao.App
 {
-    public partial class MainForm : Form
+    public partial class MainForm : dpLibrary05.fMNU_Principal
     {
         public MainForm()
         {
--- src/Dataplace.Imersao.App/Program.cs
+++ src/Dataplace.Imersao.App/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Dataplace.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,7 +19,27 @@ namespace Dataplace.Imersao.App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            var executingAssembly = new Assembly[]
+              { Assembly.GetExecutingAssembly(), typeof(Dataplace.Imersao.Core.Boot).Assembly};
+
+            var builder = Dataplace.Core.DataplaceApplication.CreateBuilder(null)
+                .UseAppName("SALESAPP")
+                .UseLayout(AppLayoutEnum.SideMenu)
+                .UseMediatR(executingAssembly);
+
+            ConfiguurationService(builder.Services);
+
+            var app = builder.Build();
+            app.Run<MainForm>();
+
         }
+
+        private static void ConfiguurationService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<MainForm>();
+        }
+
+
     }
 }
