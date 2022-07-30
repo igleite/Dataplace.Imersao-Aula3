6dc7ef1d75e2450be0f7926f4557e4e90f184127
Johannes Hermans <johannes@dataplace.com.br>
2022-07-30 10:10:26 -0300

    Ajuste report

--- src/Dataplace.Imersao.Presentation/Views/TesteReportView.Designer.cs
+++ src/Dataplace.Imersao.Presentation/Views/TesteReportView.Designer.cs
@@ -28,18 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnSystemParameters.SuspendLayout();
+            this.tpSystemParameters.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnSystemParameters
+            // 
+            this.pnSystemParameters.Controls.Add(this.textBox1);
+            this.pnSystemParameters.Size = new System.Drawing.Size(953, 232);
+            // 
+            // tpSystemParameters
+            // 
+            this.tpSystemParameters.Size = new System.Drawing.Size(953, 232);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 22);
+            this.textBox1.TabIndex = 0;
             // 
             // TesteReportView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "TesteReportView";
-            this.Size = new System.Drawing.Size(961, 290);
+            this.Size = new System.Drawing.Size(961, 342);
+            this.pnSystemParameters.ResumeLayout(false);
+            this.pnSystemParameters.PerformLayout();
+            this.tpSystemParameters.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- src/Dataplace.Imersao.Presentation/Views/TesteReportView.cs
+++ src/Dataplace.Imersao.Presentation/Views/TesteReportView.cs
@@ -12,9 +12,62 @@ namespace Dataplace.Imersao.Presentation.Views
 {
     public partial class TesteReportView : dpLibrary05.Infrastructure.UserControls.ucSymGen_ReportDialog
     {
+
+        private const string _name = "Totalização de Condições de Pagamento";
+        private const int _itemSeg = 649;
+        private const int _reportId = 1006338;
+
         public TesteReportView()
         {
             InitializeComponent();
+
+            this.ReportConfiguration += TesteReportView_ReportConfiguration;
+            this.BeforeLoadReport += TesteReportView_BeforeLoadReport;
+            this.AfterLoadReport += TesteReportView_AfterLoadReport;
+            this.LoadReport += TesteReportView_LoadReport;
         }
+
+        private void TesteReportView_ReportConfiguration(object sender, ReportConfigurationEventArgs e)
+        {
+            // configuraçõe iniciais da tela
+            // item de segurança
+            // id do report,
+
+            this.Text = _name;
+            e.ReportList.Add(_reportId,
+                           new dpLibrary05.SymphonyReport.clsSymReport.ReportData(true)
+                           {
+                               Id = _reportId,
+                               ItemSeg = _itemSeg.ToString(),
+                               Name = _name
+                           });
+            e.SecurityIdList.Add(_itemSeg);
+
+        }
+
+        private void TesteReportView_BeforeLoadReport(object sender, BeforeLoadReportEventArgs e)
+        {
+            // validações para exibir report
+            // item de segurança
+            // preenchimento dos controles
+
+        }
+
+        private void TesteReportView_LoadReport(object sender, LoadReportEventArgs e)
+        {
+            // definir o report a ser executado
+            e.ReportData = ReportList[_reportId];
+
+            // passagem dos parâmetros para o report
+            //e.ReportData.Parametros.Items.Add("cdEmpresa", dpLibrary05.mGenerico.SymPRM.cdempresa);
+            //e.ReportData.Parametros.Items.Add("cdFilial", dpLibrary05.mGenerico.SymPRM.cdfilial);
+        }
+
+        private void TesteReportView_AfterLoadReport(object sender, AfterLoadReportEventArgs e)
+        {
+            // ...
+        }
+
+
     }
 }
